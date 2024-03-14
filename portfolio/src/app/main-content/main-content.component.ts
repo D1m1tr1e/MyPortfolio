fa3b77a95b05e4ba0b505f70918d366374e35b5d
D1m1tr1e <126110046+D1m1tr1e@users.noreply.github.com>
2024-03-14 21:55:23 +0100

    added my skills section

--- portfolio/src/app/main-content/main-content.component.ts
+++ portfolio/src/app/main-content/main-content.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { MyNameSectionComponent } from './my-name-section/my-name-section.component';
-import {ArrowDownComponent } from './arrow-down/arrow-down.component';
-import {AboutMeComponent } from './about-me/about-me.component';
+import { ArrowDownComponent } from './arrow-down/arrow-down.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ArrowRightComponent } from './arrow-right/arrow-right.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [MyNameSectionComponent, ArrowDownComponent, AboutMeComponent],
+  imports: [MyNameSectionComponent, ArrowDownComponent, AboutMeComponent,
+    ArrowRightComponent, MySkillsComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
 export class MainContentComponent {
-
 }
