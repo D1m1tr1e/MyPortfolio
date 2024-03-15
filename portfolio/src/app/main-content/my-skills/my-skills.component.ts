3a9b636c3529f9c13b6cd2bc89ea7b65191f287e
D1m1tr1e <126110046+D1m1tr1e@users.noreply.github.com>
2024-03-15 23:01:30 +0100

    fixed bugs in my skills section

--- portfolio/src/app/main-content/my-skills/my-skills.component.ts
+++ portfolio/src/app/main-content/my-skills/my-skills.component.ts
@@ -1,12 +1,57 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-my-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './my-skills.component.html',
   styleUrl: './my-skills.component.scss'
 })
 export class MySkillsComponent {
+
+  skills = [
+    {
+      img: "angular.png",
+      name: "Angular"
+    },
+    {
+      img: "typescript.png",
+      name: "TypeScript"
+    },
+    {
+      img: "javascript.png",
+      name: "JavaScript"
+    },
+    {
+      img: "html.png",
+      name: "HTML"
+    },
+    {
+      img: "css.png",
+      name: "CSS"
+    },
+    {
+      img: "firebase.png",
+      name: "Firebase"
+    },
+    {
+      img: "git.png",
+      name: "Git"
+    },
+    {
+      img: "scrum.png",
+      name: "Scrum"
+    },
+    {
+      img: "api.png",
+      name: "Angular"
+    },
+    {
+      img: "materialdesign.png",
+      name: "Material Design"
+    }
+
+  ]
 
 }
