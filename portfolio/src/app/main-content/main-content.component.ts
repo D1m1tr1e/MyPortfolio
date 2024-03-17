139304bb17bf2934b44686de65d19c65c61e19c3
D1m1tr1e <126110046+D1m1tr1e@users.noreply.github.com>
2024-03-17 21:47:24 +0100

    added footer

--- portfolio/src/app/main-content/main-content.component.ts
+++ portfolio/src/app/main-content/main-content.component.ts
@@ -5,13 +5,15 @@ import { AboutMeComponent } from './about-me/about-me.component';
 import { ArrowRightComponent } from './arrow-right/arrow-right.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
 import { ArrowLeftComponent } from './arrow-left/arrow-left.component';
-import {MyProjectsComponent} from './my-projects/my-projects.component';
+import { MyProjectsComponent } from './my-projects/my-projects.component';
+import { ContactFormComponent } from './contact-form/contact-form.component'
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
   imports: [MyNameSectionComponent, ArrowDownComponent, AboutMeComponent,
-    ArrowRightComponent, MySkillsComponent, ArrowLeftComponent, MyProjectsComponent],
+    ArrowRightComponent, MySkillsComponent, ArrowLeftComponent,
+    MyProjectsComponent, ContactFormComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
