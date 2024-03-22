c04a2c260017467a7cdd737f283839f7f7455481
D1m1tr1e <126110046+D1m1tr1e@users.noreply.github.com>
2024-03-22 17:14:45 +0100

    fixed bugs

--- portfolio/src/app/shared/header/header.component.ts
+++ portfolio/src/app/shared/header/header.component.ts
@@ -1,20 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  menuIsOpen = false;
 
   openMenu() {
-    let menuElement = document.getElementById('menu');
+    let menuElement = document.getElementById('menu-btn');
     if (menuElement) {
       menuElement.classList.toggle("change");
     }
-    
   }
-
 }
+
+
