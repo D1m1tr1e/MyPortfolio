8842d2419a631b7433d4eb01fe23ea4b6def0113
D1m1tr1e <126110046+D1m1tr1e@users.noreply.github.com>
2024-03-23 15:27:41 +0100

    arrow right animation completed

--- portfolio/src/app/main-content/arrow-right/arrow-right.component.ts
+++ portfolio/src/app/main-content/arrow-right/arrow-right.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-arrow-right',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './arrow-right.component.html',
   styleUrl: './arrow-right.component.scss'
 })
 export class ArrowRightComponent {
+
+  /*startAnimationArrow() {
+    document.getElementById('arrowMove')?.classList.add('startMovingArrow');
+  }*/
 
 }
