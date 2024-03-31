de4cca55463ab7a76550f13986815235891e177b
D1m1tr1e <126110046+D1m1tr1e@users.noreply.github.com>
2024-03-31 23:52:52 +0200

    working on form validation

--- portfolio/src/app/main-content/contact-form/contact-form.component.ts
+++ portfolio/src/app/main-content/contact-form/contact-form.component.ts
@@ -1,27 +1,45 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.scss'
 })
 export class ContactFormComponent {
 
   checkboxIsChecked = false;
+  sendButtonClicked = false;
 
   acceptPolicy() {
-    let checkbox = document.getElementById("checkbox") as HTMLInputElement;;
-    if (checkbox) {
-      checkbox.addEventListener("change", () => {
-        if (checkbox.checked) {
-          console.log('ok');
-        } else {
-          console.log('nok');
-        }
-      });
+    this.checkboxIsChecked = !this.checkboxIsChecked;
+    console.log(this.checkboxIsChecked);
+  }
+
+  sendMessage() {
+    this.sendButtonClicked = true;
+  }
+
+  contactData = {
+    name: "",
+    email: "",
+    message: ""
+  }
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.valid && ngForm.submitted) {
+      console.log(this.contactData)
+      /* this.contactData = {
+         name: "",
+         email: "",
+         message: ""
+       };
+       ngForm.resetForm(); // Zurücksetzen des Formulars
+ */
     }
   }
+
 
 }
